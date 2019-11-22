39c8434d91a6fab80d9fa5d1487874d60cbeb824
shinyay <shinya.com@gmail.com>
2019-11-22 20:56:03 +0900

    Modify parameter using Value annotation retlieving property and initial value

--- hello-azure/src/main/kotlin/io/pivotal/shinyay/controller/HelloAzureController.kt
+++ hello-azure/src/main/kotlin/io/pivotal/shinyay/controller/HelloAzureController.kt
@@ -1,11 +1,12 @@
 package io.pivotal.shinyay.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloAzureController {
+class HelloAzureController(@Value("\${application.message:Non Configuration by Spring Cloud Config}") var message: String) {
 
     @GetMapping("/hello")
-    fun hello() = "Hello, Azure Spring Cloud"
+    fun hello() = message
 }
