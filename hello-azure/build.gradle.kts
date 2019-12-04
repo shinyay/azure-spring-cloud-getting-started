9a2c1a4276e25c49c50f265ec73d660063de3128
shinyay <shinya.com@gmail.com>
2019-12-05 07:31:37 +0900

    Add webflux starter dependency

--- hello-azure/build.gradle.kts
+++ hello-azure/build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "Hoxton.RC2"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
